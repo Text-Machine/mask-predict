2135d8ef1386197153717db0dc5797853af6b84c
Kaspar Beelen <kaspar.beelen@sas.ac.uk>
2026-03-24 14:54:34 +0100

    fix import bug

--- explain/__init__.py
+++ explain/__init__.py
@@ -1,6 +1,6 @@
-from .tools import pick_device, parse_pred_column, build_texts_targets
+from .tools import pick_device, parse_pred_column, build_texts_targets,result_as_dataframe
 from .explain import MaskedLMExplainer, compare_explainers
-from .analyse import summarize_top_predictors, analyze_comparison, result_as_dataframe
+from .analyse import summarize_top_predictors, analyze_comparison
 from .visualise import highlight_context_tokens, render_top_shift_sentences, plot_model_comparison_bar, plot_scatter_model_comparison
 
 
